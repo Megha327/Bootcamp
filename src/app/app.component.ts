9ef68c2ce556a587a180d4e3e05a63e15fd90519
Megha327 <megha@tothenew.com>
2020-03-25 23:08:09 +0530

    innitial commit of angular forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm, MaxLengthValidator } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,52 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Bootcamp';
+  @ViewChild('f') signupForm:NgForm;
+  DefaultSelected="noida";
+  answer = '';
+  genders=["Male","Female","Others"];
+  user = {
+    email: '',
+    password: '',
+    gender: '',
+    city: '',
+    file:''
+  };
+  submitted = false;
+
+  // onSubmit(form:NgForm){
+  //   console.log(form);
+  // }
+
+  onSubmit(){
+    this.submitted = true;
+    // console.log(this.signupForm);
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.password = this.signupForm.value.userData.password;
+    this.user.gender = this.signupForm.value.userData.gender;
+    this.user.city = this.signupForm.value.userData.city;
+    this.user.file = this.signupForm.value.userData.file;  
+    this.signupForm.reset();  
+  }
+
+  onSugessted(){
+    const sugesstedmail = "example@gmail.com";
+    // this.signupForm.setValue({
+    //   userData:{
+    //     email: sugesstedmail,
+    //     password:1234,
+    //     gender:'Male',
+    //     city:'Noida',
+    //     file:'',
+    //     feedback:''
+    //   }
+    // });
+    this.signupForm.form.patchValue({
+      userData:{
+        email: sugesstedmail
+      }
+    });
+  }
+
 }
+
---
+++ src/app/reactive-form/reactive-form.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-reactive-form',
+  templateUrl: './reactive-form.component.html',
+  styleUrls: ['./reactive-form.component.css']
+})
+export class ReactiveFormComponent implements OnInit {
+  genders=["Male","Female","Others"];
+  signupform:FormGroup;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.signupform = new FormGroup({
+      'mail': new FormControl(null, [Validators.required, Validators.email]),
+      'paswrd': new FormControl(null, [Validators.required, Validators.maxLength(6)]),
+      'gen': new FormControl('Female'),
+      'cit': new FormControl('Noida', Validators.required),
+      'fil': new FormControl(null, Validators.required)
+    });
+  }
+
+  onSubmit(){
+    console.log(this.signupform);
+  }
+
+}
