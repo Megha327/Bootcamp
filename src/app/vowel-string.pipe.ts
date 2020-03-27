b5c0c8c7aefbe93e0212bf987b1a145ea5e5f909
Megha327 <megha@tothenew.com>
2020-03-27 14:45:20 +0530

    commit chnages in vowelString pipe and app.component

--- src/app/vowel-string.pipe.ts
+++ src/app/vowel-string.pipe.ts
@@ -1,19 +1,22 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } from 'constants';
 
 @Pipe({
   name: 'vowelString'
 })
 export class VowelStringPipe implements PipeTransform {
 
-  transform(value:string, arr, vowel:object){
-    arr = Array.from(value.toLowerCase());
+  transform(value:string){
+    let vowel = {a:0,e:0,i:0,o:0,u:0};
+    let arr = Array.from(value.toLowerCase());
     for(let i=0; i < arr.length; i++){
       if (arr[i] in vowel) {
         //console.log("Aaya - " + strArr[i]);
         vowel[arr[i]] += 1;
       }
     }
-    return vowel;
+    return JSON.stringify(vowel);
+    //return vowel;
   }
 
 }
