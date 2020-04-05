5bdb0064fcc78441fd152f673a932fb3046df6be
Megha327 <megha@tothenew.com>
2020-04-05 16:25:59 +0530

    commit chnages

--- OwnPracticeAngularProject/src/app/app.module.ts
+++ OwnPracticeAngularProject/src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipeBookComponent } from './recipe-book/recipe-book.component';
+import { RecipeDetailComponent } from './recipe-book/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipe-book/recipe-list/recipe-item/recipe-item.component';
+import { RecipeListComponent } from './recipe-book/recipe-list/recipe-list.component';
+import { ShopingListComponent } from './shoping-list/shoping-list.component';
+import { ShoppingEditComponent } from './shoping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
+    RecipeBookComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    RecipeListComponent,
+    ShopingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule,
---
+++ OwnPracticeAngularProject/src/app/shoping-list/shoping-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Ingredients } from '../shared/ingredient.model';
+
+@Component({
+  selector: 'app-shoping-list',
+  templateUrl: './shoping-list.component.html',
+  styleUrls: ['./shoping-list.component.css']
+})
+export class ShopingListComponent implements OnInit {
+  ingredients: Ingredients[] = [
+      new Ingredients('Apple',5),
+      new Ingredients('mango', 10)
+  ];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
